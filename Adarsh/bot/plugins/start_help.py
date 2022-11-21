0dcdc667df07cb6e0c76e72b3b6da0f5193bd700
Dengerous <99312935+dengerous53@users.noreply.github.com>
2022-11-21 16:33:35 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -4,7 +4,7 @@ from pyrogram import filters, emoji
 from Adarsh.bot import StreamBot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-@StreamBot.on_message(filters.command(['start', 'help']))
+@StreamBot.on_message(filters.command(['start']))
 async def start(_, m: Message):
     await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
                 
