5e6ba0e44ac8919e7c47b2d47ce8d171ca6a9387
Dengerous <99312935+dengerous53@users.noreply.github.com>
2022-11-21 16:59:44 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -6,6 +6,12 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start']))
 async def start(_, m: Message):
-    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get Download stream link.',
                 
                   )
+
+@StreamBot.on_message(filters.command(['help']))
+async def help(_, m: Message):
+    await m.reply(f'i can convert any file into Download or Streaming link, even more then 2GB files 😉. \n18+ Content Not Allowed So please Avoid it ⚠️. \nLinks are Permanent 🤗.',
+                  )
+    
