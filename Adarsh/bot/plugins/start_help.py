6efa104967ecf306259933d3a707cc2f378d1b01
Shubhamxd82 <119496423+Shubhamxd82@users.noreply.github.com>
2022-12-05 00:30:26 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -6,20 +6,21 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start']))
 async def start(_, m: Message):
-    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get Download stream link.',
+    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get Download stream link. \n\nIf You Want Help Press /help',
                 
                   )
 
 @StreamBot.on_message(filters.command(['help']))
 async def help(_, m: Message):
-    await m.reply(f'i can convert any file into Download Link or Online Streaming Link! \n2GB+ files Supported ✅ \n18+ Content Not Allowed ⚠️ \nLinks are Permanent 🍀',
-
+    await m.reply(f'i can convert any file into Download Link or Online Streaming Link! \n2GB+ files Supported ✅ \n18+ Content Not Allowed ⚠️ \nLinks are Permanent 🍀 \n\nFor More Help Press /players',
+    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Any Queries DM Here ✅', url='https://t.me/Link_Reporter_Bot')]])
+                  
                   )
     
 @StreamBot.on_message(filters.command(['about']))
 async def about(_, m: Message):
     await m.reply(f'✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: Flash \n✯ 𝙼𝚈 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: Shubham \n✯ 𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈: [Moviesss4ers](https://t.me/Moviesss4ers) \n✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼 \n✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: Python \n✯ 𝙳𝙰𝚃𝙰𝙱𝙰𝚂𝙴: Free \n✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: Koyeb',
-    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('My Owner 😎', url='https://t.me/Link_Reporter_Bot')]])
+    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('My Owner 😎', url='https://t.me/Nexus_Shubhu')]])
                   
                   )
 
