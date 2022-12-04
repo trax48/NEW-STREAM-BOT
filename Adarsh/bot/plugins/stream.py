5b43b915521185e1565af6c24fe052e14918281a
Shubhamxd82 <119496423+Shubhamxd82@users.noreply.github.com>
2022-12-04 22:12:01 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -108,8 +108,8 @@ async def private_receive_handler(c: Client, m: Message):
             quote=True,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("STREAM 🖥", url=stream_link), #Stream Link
-                                               [InlineKeyboardButton('DOWNLOAD 📥', url=online_link), #Download Link
-                                               [InlineKeyboardButton("STREAM 🖥", url=stream_link2)]]), #Stream Link
+                                                InlineKeyboardButton('DOWNLOAD 📥', url=online_link), #Download Link
+                                                InlineKeyboardButton("STREAM 🖥", url=stream_link2)]]), #Stream Link 2
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
